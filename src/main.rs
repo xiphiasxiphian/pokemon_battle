5d62c6bbbca67b2fc465aaaab0eace60f0e29db1
Sophie Holman <xiphias.xiphian@gmail.com>
2026-09-05 22:30:08 +0100

    bit of a late init

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::path::Path;
+
+use color_eyre::eyre::{self, OptionExt};
+
+use crate::pokemon::manager::PokemonManager;
+
+pub mod pokemon;
+
+fn main() -> eyre::Result<()>
+{
+    let manager = PokemonManager::new(Path::new("./assets/pokemon/"))?;
+    let sylveon = manager.spawn("SYLVEON", 50).ok_or_eyre("Failed to get Sylveon")?.build();
+    println!("{:#?}", sylveon);
+
+    Ok(())
+}
